9b2587e785b28981f7e20f42ac5d63492994ac96
Archit Khode <arkits@outlook.com>
2021-09-04 23:29:56 -0700

    ci: added workflow and hostscript

---
+++ scripts/deploy-hostscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# deploy-hostscript.sh
+
+set -e
+
+echo ">>> hostscript running as $(whoami)@$(hostname)"
+
+cd /home/arkits/software/telegaram/
+
+echo "==> Build haldi - Frontend"
+cd haldi
+npm install
+npm run build
+cd ..
+
+echo "==> Build mirchi - Backend"
+cd mirchi
+npm install
+
+echo "==> Add haldi to mirchi - copy frontend to server"
+npm run addHaldi
+
+echo "==> DB migration"
+npx prisma migrate deploy
+
+echo "==> Start the server"
+npm start
